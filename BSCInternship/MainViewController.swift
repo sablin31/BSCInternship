1f05b85b077032e629d9ddc339c32ed21555ce96
Alex Sablin <sablin_lex@icloud.com>
2022-03-27 08:56:18 +0300

    Create repo

--- BSCInternship/MainViewController.swift
+++ BSCInternship/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  BSCInternship
 //
 //  Created by Алексей Саблин on 27.03.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
 
