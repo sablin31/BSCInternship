d5058bbdaca2c3d96d042ca3ab44c0d80aca4813
Alex Sablin <sablin_lex@icloud.com>
2022-03-27 08:51:48 +0300

    Initial Commit

---
+++ BSCInternship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BSCInternship
+//
+//  Created by Алексей Саблин on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
